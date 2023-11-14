976e4ce07a4089f6f45689cdda0573f2908debb2
bdan3095 <150065481+bdan3095@users.noreply.github.com>
2023-11-14 23:04:13 +0200

    Update #10.py

--- Lesson_1/#10.py
+++ Lesson_1/#10.py
@@ -1,3 +1,8 @@
+# 10 Школярі та яблука
+# n школярів ділять k яблук порівну, недільний залишок залишається в кошику.
+# Скільки яблук дістанеться кожному школярові та скільки залишиться в кошику?
+# Програма отримує на вхід числа n і k - цілі, додатні, не перевищують 10000.
+
 k = int(input("Please enter the number of apples: "))
 assert 1 < k < 10001, "The value entered is incorrect"
 n = int(input("Please enter the number of kids: "))
