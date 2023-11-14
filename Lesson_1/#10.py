31f8d6a55232c6c4db9fc24eaa0de9dd94bff46d
bdan3095 <150065481+bdan3095@users.noreply.github.com>
2023-11-14 15:14:52 +0200

    Update #10.py

--- Lesson_1/#10.py
+++ Lesson_1/#10.py
@@ -1,1 +1,6 @@
+k = int(input("Please enter the number of apples: "))
+assert 1 < k < 10001, "The value entered is incorrect"
+n = int(input("Please enter the number of kids: "))
+assert 1 < n < 10001, "The value entered is incorrect"
 
+print("The number of apples left: ",k % n)
