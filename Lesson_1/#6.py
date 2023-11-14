6366dc89d893d5c968ae1f84e181aa454cfa0835
bdan3095 <150065481+bdan3095@users.noreply.github.com>
2023-11-14 14:38:00 +0200

    Update #6.py

--- Lesson_1/#6.py
+++ Lesson_1/#6.py
@@ -1,1 +1,6 @@
+var_1 = int(input("What's your year of birth?: "))
+var_2 = str(input("What's your name?: "))
+var_3 = int(input("What's year today?: "))
 
+print("Your name is: ",var_2)
+print("You are ",var_3 - var_1," years old!")
