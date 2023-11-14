3d1918aaa5c2f277eff592abecd74b7f7b35c0cf
bdan3095 <150065481+bdan3095@users.noreply.github.com>
2023-11-14 23:03:02 +0200

    Update #7.py

--- Lesson_1/#7.py
+++ Lesson_1/#7.py
@@ -1,3 +1,5 @@
+# 7 Перевод з цельсія в фаренгейт:
+# Напишіть програму, яка запитує в користувача кількість градусів в цельсіях і повертає в фаренгейтах.
 var_1 = int(input("Please enter the temperature in Celsius: "))
 
 print("The temperature in Farenheit is: ", var_1*9/5+32)
