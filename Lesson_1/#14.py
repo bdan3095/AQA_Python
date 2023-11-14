8a7ca435c70521835e0536ee1fe60c0e30c75e68
bdan3095 <150065481+bdan3095@users.noreply.github.com>
2023-11-14 16:17:30 +0200

    Update #14.py

--- Lesson_1/#14.py
+++ Lesson_1/#14.py
@@ -1,1 +1,7 @@
-
+salary = float(input("Enter your monthly salary: "))
+salary_quarter = (salary*3)
+percent_1 = 0.18
+military_1 = 0.015
+social_1 = 0.22
+total_taxation = (salary_quarter*(percent_1+military_1+social_1))
+print("Total taxes for 3 months fiscal period: $", (total_taxation))
