bab01a59db7b0e73ea2190febf9133e75406a1f5
bdan3095 <150065481+bdan3095@users.noreply.github.com>
2023-11-14 14:32:00 +0200

    Update #5.py

--- Lesson_1/#5.py
+++ Lesson_1/#5.py
@@ -1,1 +1,6 @@
+var_1 = int(input("Enter the number #1: "))
+var_2 = int(input("Enter the number#2: "))
+var_3 = str(var_1) + str(var_2)
 
+print("The sum is: ",var_1 + var_2)
+print("Adding: ",var_3)
